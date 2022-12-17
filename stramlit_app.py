883eaf0d76d226e6554624d938b9552124d746cd
Venura Perera <55928037+Venurap@users.noreply.github.com>
2022-12-17 23:10:35 +1100

    Updated Emojis

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
 
+streamlit.title('                             ')
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 2 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣  Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞  Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
