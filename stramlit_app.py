8c8eb1bc7fe291e470952414ac77a345fa23dd61
Venura Perera <55928037+Venurap@users.noreply.github.com>
2022-12-17 23:15:55 +1100

    Add header emoji

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('                             😺')
+streamlit.title('                                                              😺')
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
