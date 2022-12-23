39a4a1aeccb7150f0e38e8e64724a406356896cd
Venura Perera <55928037+Venurap@users.noreply.github.com>
2022-12-23 16:03:23 +1100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -49,6 +49,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -59,8 +61,6 @@ my_data_row = my_cur.fetchall()
 # streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-
-streamlit.stop()
 
 fruit_choice2 = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_choice2)
